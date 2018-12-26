ceb218351b30c55598f74c0e88a66a0d0e6f6432
rafal_ubuntu_praca <rafal@gmail>
2018-12-26 17:53:10 +0100

    testy clienta

--- src/test/java/boot_jpa_listopad/RestClientServiceUserTest.java
+++ src/test/java/boot_jpa_listopad/RestClientServiceUserTest.java
@@ -1,39 +1,49 @@
 package boot_jpa_listopad;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
 import static org.junit.Assert.assertNotNull;
 
-import org.hamcrest.*;
-import org.hamcrest.core.IsNot;
-import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-import boot_jpa_listopad.JsonPlaceHolderRestClient.JsonPlaceHolderUserServiceImpl;
+import boot_jpa_listopad.JsonPlaceHolderRestClient.JsonPlaceHolderUserService;
 import lombok.extern.slf4j.Slf4j;
 
 
-//czy musze tego runnera uzyc, skoro nie korzytam z autowired?
-//@RunWith(SpringJUnit4ClassRunner.class)
+/**
+ * czy musze tego runnera uzyc, skoro nie korzytam z autowired? 
+ * testy powinny byc odpalane bez springa, wiec jeśli bym nie korzystac z autowired to nie potrzeba runnera.
+ * Jednak musze skorzystac z url-a, ktory jest w spring.properties. Dlatego potrzebny jest runner i dodatkowo springboottest
+ * The @SpringBootTest annotation tells Spring Boot to go and look for a main configuration class (one with @SpringBootApplication for instance), and use that to start a Spring application context. 
+ * https://spring.io/guides/gs/testing-web/
+ * 
+ * dopisac test, ktory bedzie korzystal z propertisow, ktore beda mialy mocka -test.props
+ * https://www.logicbig.com/tutorials/spring-framework/spring-core/test-property-source-annotation.html
+ * @author rafal
+ *
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
 @Slf4j
 public class RestClientServiceUserTest {
 
+	@Autowired
+	private JsonPlaceHolderUserService jsonPlaceHolderUserService;
 	
-	private JsonPlaceHolderUserServiceImpl service;
 	
-	
-	@Before
-	public void setUp() {
-		service = new JsonPlaceHolderUserServiceImpl();
-	}
+//	@Before
+//	public void setUp() {
+//		service = new JsonPlaceHolderUserServiceImpl();
+//	}
 	
 	@Test
 	public void testGetJsonPlaceHolderUser() {
 		
 	//zmienic na assercje z hamcresta	
-		log.info("======================== "+service.getJsonPlaceHolderUser().toString());
-		assertNotNull(service.getJsonPlaceHolderUser());
+		log.info("======================== "+jsonPlaceHolderUserService.getJsonPlaceHolderUser().toString());
+		assertNotNull(jsonPlaceHolderUserService.getJsonPlaceHolderUser());
 	}
 
 }

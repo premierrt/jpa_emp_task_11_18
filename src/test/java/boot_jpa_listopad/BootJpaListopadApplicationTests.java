3de5165b12a87355a3b30f6c1510bccde37d5467
rafal_ubuntu_praca <rafal@gmail>
2018-12-26 18:39:17 +0100

    usuniecie szablonowego testu springboottest

--- src/test/java/boot_jpa_listopad/BootJpaListopadApplicationTests.java
+++ src/test/java/boot_jpa_listopad/BootJpaListopadApplicationTests.java
@@ -5,8 +5,9 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+// rt - komentuje zeby nie uruchamiac springa i przez to przyspieszyc testy
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class BootJpaListopadApplicationTests {
 
 	@Test

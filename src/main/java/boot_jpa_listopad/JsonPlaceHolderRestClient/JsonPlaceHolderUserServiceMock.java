758e50a183ef086c12e72266661b834d2d58079a
dom_linux <dom_linux@ddddd.xy>
2019-01-04 23:41:53 +0100

    mock1

---
+++ src/main/java/boot_jpa_listopad/JsonPlaceHolderRestClient/JsonPlaceHolderUserServiceMock.java
@@ -0,0 +1,33 @@
+package boot_jpa_listopad.JsonPlaceHolderRestClient;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Service
+@Slf4j
+public class JsonPlaceHolderUserServiceMock implements JsonPlaceHolderUserService {
+
+	@Override
+	public List<JsonPlaceHolderUser> getJsonPlaceHolderUser() {
+		JsonPlaceHolderUser mockUser = new JsonPlaceHolderUser();
+		mockUser.setEmail("mockEmail");
+		mockUser.setId(1L);
+		mockUser.setName("MockName");
+		mockUser.setUsername("MockUserName");
+	    ArrayList<JsonPlaceHolderUser> al= new ArrayList<JsonPlaceHolderUser>();
+	    al.add(mockUser);
+	    log.info("============== Mock Rest Service get returns: "+al.toString());
+	    return al;
+	}
+
+	@Override
+	public JsonPlaceHolderPostDTO setPost(JsonPlaceHolderPostDTO jsonPlaceHolderPostDTO) {
+		log.info("============= mock rest service post. returns: "+jsonPlaceHolderPostDTO.toString());
+		return jsonPlaceHolderPostDTO;
+	}
+
+}
